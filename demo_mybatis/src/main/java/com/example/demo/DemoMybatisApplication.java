fd60db0869ca3261cf74043c5e44b9dbf86d216c
wojiushiwo <2590404747@qq.com>
2017-06-02 09:47:07 +0800

    提交文件到暂存区

--- demo_mybatis/src/main/java/com/example/demo/DemoMybatisApplication.java
+++ demo_mybatis/src/main/java/com/example/demo/DemoMybatisApplication.java
@@ -3,14 +3,13 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
 public class DemoMybatisApplication {
 
 	public static void main(String[] args) {
-		System.out.println("111");
+		System.out.println("12222");
 		SpringApplication.run(DemoMybatisApplication.class, args);
 	}
 }
